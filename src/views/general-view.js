a8a8d0aa7112d6a868db4638acef1aab2e8f2999
Carolina Casas <karopcasas@gmail.com>
2024-07-24 13:09:41 -0600

    add folders to views

---
+++ src/views/general-view.js
@@ -0,0 +1,20 @@
+import {LitElement, html} from 'lit';
+
+export class GeneralView extends LitElement {
+  static properties = {
+    version: {},
+  };
+
+  constructor() {
+    super();
+    this.version = 'STARTING';
+  }
+
+  render() {
+    return html`
+    <p>Welcome to the Lit tutorial!</p>
+    <p>This is the ${this.version} code.</p>
+    `;
+  }
+}
+customElements.define('general-view', GeneralView);
